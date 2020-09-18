21b73a4d4609fa73152f1389d49d1ff035c560f2
Rodrigo <rodgomesc@gmail.com>
2020-09-18 11:09:46 -0300

    fix(fix firebase): fix firebase permission errors

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -11,8 +11,8 @@ const app = firebase.initializeApp({
   projectId: 'weaver-reports',
   storageBucket: 'weaver-reports.appspot.com',
   messagingSenderId: '711746354089',
-  appId: '1:711746354089:web:970bbc7faa2cdb1088a2b8',
-  measurementId: 'G-FV1DNJMN01',
+  appId: '1:711746354089:web:cc7a1fed63f881a288a2b8',
+  measurementId: 'G-BZZ965RVDN',
 });
 // Initialize Firebase
 
